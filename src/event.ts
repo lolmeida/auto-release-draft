13b145d39ce9e98ef036b3cf28e17f40189b985b
lolmeida <lolmeida@gmail.com>
2024-04-24 22:16:13 +0100

    Running command line programs

--- src/event.ts
+++ src/event.ts
@@ -2,7 +2,7 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 export function getCreatedTag(): string | null {
-  /*  if (github.context.eventName !== 'create') {
+  if (github.context.eventName !== 'create') {
     core.info(`The event name was ${github.context.eventName}`)
     return null
   }
@@ -10,6 +10,6 @@ export function getCreatedTag(): string | null {
   if (github.context.payload.ref_type !== 'tag') {
     core.info('The created reference was a branch, not a tag')
     return null
-  } */
+  }
   return github.context.payload.ref
 }
