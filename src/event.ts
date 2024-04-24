d0872ff2bbf8dc8f9c7a77545ff2916a228dd6cc
lolmeida <lolmeida@gmail.com>
2024-04-24 21:03:50 +0100

    Running command line programs

--- src/event.ts
+++ src/event.ts
@@ -3,12 +3,12 @@ import * as github from '@actions/github'
 
 export function getCreatedTag(): string | null {
   core.info('github.context.eventName:')
-  if (github.context.eventName != 'create') {
+  if (github.context.eventName.toLocaleLowerCase() !== 'create') {
     core.info(`The event name was ${github.context.eventName}`)
     return null
   }
 
-  if (github.context.payload.ref_type != 'tag') {
+  if (github.context.payload.ref_type.toLowerCase() !== 'tag') {
     core.info('The created reference was a branch, not a tag')
     return null
   }
