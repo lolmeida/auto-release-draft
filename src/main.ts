850592638c3c485b534456fdf9fae202837c110c
lolmeida <lolmeida@gmail.com>
2024-04-24 16:46:17 +0100

    added event, semver and updated main

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,20 @@
 import * as core from '@actions/core'
+import * as event from './event'
+import * as version from './version'
 
-/**
- * The main function for the action.
- * @returns {Promise<void>} Resolves when the action is complete.
- */
+
 export async function run(): Promise<void> {
   try {
-    // Set outputs for other workflow steps to use
+
+    const tag = event.getCreatedTag();
+
+    if ( tag && version.isSemVer(tag)) {
+      // TODO: Implement the rest of the workflow
+    }
+
+
     core.setOutput('release-url', 'https://example.com/')
   } catch (error) {
-    // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
   }
 }
---
+++ src/semver.ts
@@ -0,0 +1,14 @@
+import * as semver from 'semver';
+
+export function isSemVer(version: string): boolean {
+    return semver.valid(version) !== null;
+    }
+
+export function isPreRelease(version: string): boolean {
+    return semver.prerelease(version) !== null;
+}
+
+export function removePrefix(version: string): string {
+  const parsedVersion = semver.parse(version);
+    return parsedVersion ? parsedVersion.version : version;
+}
