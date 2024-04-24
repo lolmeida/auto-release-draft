b823d7edb78aa0c80ea37bc7c82c0846a3392a47
lolmeida <lolmeida@gmail.com>
2024-04-24 18:32:40 +0100

    Running command line programs

--- src/markdown.ts
+++ src/markdown.ts
@@ -1,3 +1,3 @@
-export function toUnorderedList(items) {
-  return items.map(item => `- ${item}`).join('\n')
+export function toUnorderedList(items: string[]) {
+return items.map((item: string) => `- ${item}`).join('\n')
 }
