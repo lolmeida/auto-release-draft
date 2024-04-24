f98f91d525e5361a53ef8f68b149a319040ad579
lolmeida <lolmeida@gmail.com>
2024-04-24 22:00:43 +0100

    Running command line programs

--- src/event.ts
+++ src/event.ts
@@ -3,7 +3,7 @@ import * as github from '@actions/github'
 
 export function getCreatedTag(): string | null {
   if (github.context.eventName !== 'create') {
-    core.info(`The event name was ${github.context.eventName}\n${github.context.payload.toString()}`)
+    core.info(`The event name XXXX was ${github.context.eventName}`)
     return null
   }
 
