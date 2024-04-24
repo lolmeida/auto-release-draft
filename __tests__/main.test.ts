e2397d463f2e77e32546dfd004ebf682cb8a769a
lolmeida <lolmeida@gmail.com>
2024-04-24 15:19:52 +0100

    fixed main.test.ts

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -1,19 +1,15 @@
-import * as core from '@actions/core';
-import { run } from '../src/main';
+import * as core from '@actions/core'
+import { run } from '../src/main'
 
-jest.mock('@actions/core');
+jest.mock('@actions/core')
 
 describe('When running in action', () => {
-  const fakeSetOutput = core.setOutput as jest.MockedFunction<typeof core.setOutput>;
-  
+  const fakeSetOutput = core.setOutput as jest.MockedFunction<
+    typeof core.setOutput
+  >
+
   test('it sets the release URL output', async () => {
-    // Arrange
-    const expectedUrl = expect.anything;
-
-    // Act
-    await run();
-
-    // Assert
-    expect(fakeSetOutput).toHaveBeenCalledWith('release-url', expectedUrl);
+    await run()
+    expect(fakeSetOutput).toHaveBeenCalledWith('release-url', expect.anything())
   })
 })
